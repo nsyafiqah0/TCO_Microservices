2c8026c3baa547e4a6ed4c01ffbb8661297ae277
nsyafiqah0 <95832450+nsyafiqah0@users.noreply.github.com>
2022-02-21 04:53:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.post('/submit-data', function (req, res) {
     var sql = 'SELECT * FROM customer WHERE custUsername = ? OR custPwd = ?';
     con.query(sql, [username, pass], function (err, result) {
     if (err) throw err;
-      var custID = res.body.custID;
+      var custID = res.custID;
       //req.setAttribute("SES_ID",custID);
       //var c = JSON.parse(this.res)
       
