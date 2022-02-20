552d47e008ead9adba6935c75aa1db20bacef830
nsyafiqah0 <95832450+nsyafiqah0@users.noreply.github.com>
2022-02-20 18:30:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -76,10 +76,6 @@ app.post('/view', function (req, res) {
    // console.log('Node server is running..');
 //});
 
-//port (as described above) and host are both wrong
-const host = 'localhost';
-const port = 3000;
-
 //use alternate localhost and the port Heroku assigns to $PORT
 const host = '0.0.0.0';
 const port = process.env.PORT || 3000;
