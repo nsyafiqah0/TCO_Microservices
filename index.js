9f3cf4b892b338829b56a44076cc6281b3ff9aee
nsyafiqah0 <95832450+nsyafiqah0@users.noreply.github.com>
2022-02-20 22:50:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.post('/submit-data', function (req, res) {
     if (err) throw err;
       res.sendFile('sucessfullylogin.html', { root:__dirname });
       //window.location = "https://takacastoff-3.herokuapp.com/";
+      window.location.href = "https://takacastoff-3.herokuapp.com/";
     });
 
 });
